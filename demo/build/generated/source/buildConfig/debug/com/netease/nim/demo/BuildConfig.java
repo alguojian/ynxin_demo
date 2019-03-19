0251e22ba170db2b78f07cf5a5608748a5865b89
ALguojian <3324883037@qq.com>
2019-03-19 20:08:27 +0800

    ..

---
+++ demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netease.nim.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netease.nim.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 63;
+  public static final String VERSION_NAME = "6.2.0";
+  // Fields from default config.
+  public static final String BUILD_DATE = "2019-3-19 15:42:00";
+  public static final String GIT_REVISION = "";
+}
